d51224d2ea9ea85af02d279534bcbecfb033a34c
hyperxpizza <hyperxpizza@gmail.com>
2020-04-02 11:44:34 +0200

    fix connecting to mongodb

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -4,41 +4,53 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+type DBClient struct {
+	*mongo.Client
+}
 
 // ConnectToDB connects server with MongoDB, returns database client
 func ConnectToDB() *mongo.Client {
 
 	//Database config
-	clientOptions := mongo.Client().ApplyUsi("mongodb_url")
-	client, err := mongo.NewClient(clientOptions)
+	client, err := mongo.NewClient(options.Client()).ApplyURI("mongo_uri_here")
 	if err != nil {
 		log.Fatalf("[-] MongoDB NewClient error: %v", err)
 	}
 
-	//Ping connection
-	err = client.Ping(context.Background(), nil)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
 	if err != nil {
-		log.Fatalf("[-] client.Ping error: %v", err)
-	} else {
-		fmt.Println("[+] Connected to the database")
+		log.Fatalf("[-] MongoDB client.Connect error: %v", err)
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil{
+		log.Fatalf("[-] Ping error: %v", err)
 	}
 
 	return client
-
+	
 }
 
-func RegisterUserinDB() {
+func (c *DBClient) InsertUser() {
 
 }
 
 // GetExpectedPassword queries password from the database based on given username
-func GetExpectedPassword(username string) string {
+func (c *DBClient) GetExpectedPassword(username string) string {
+	collection := c.Database("korero").Collection("users")
+	if err = collection.FindOne()
 
 }
 
-func CheckIfUsernameTaken(username string) bool {
-
+// CheckIfUsernameTakes queries given username from the database and check if such entry already exists
+func (c *DBClient) CheckIfUsernameTaken(username string) bool {
+	collection := c.Database("korero").Collection("users")
 }
