dfafa7ce7759ff90cd1e39eb6dedbdc92bf4cc80
hyperxpizza <hyperxpizza@gmail.com>
2020-04-02 13:53:15 +0200

    set up router and api functions

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -1,1 +1,15 @@
 package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/korero-chat/backend/pkg/api"
+)
+
+// SetRouter Creates mux Router instance to handle api routes
+func SetRouter() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/newuser", api.RegisterUserEndpoint).Methods("POST")
+
+	return router
+}
