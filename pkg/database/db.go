d4504ff17b18a466f29878b9b092b623da532a7a
hyperxpizza <hyperxpizza@gmail.com>
2020-04-02 14:12:58 +0200

    GetExpectedPasswordHash function

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -2,10 +2,10 @@ package database
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -31,26 +31,44 @@ func ConnectToDB() *mongo.Client {
 	}
 
 	err = client.Ping(ctx, readpref.Primary())
-	if err != nil{
+	if err != nil {
 		log.Fatalf("[-] Ping error: %v", err)
 	}
 
 	return client
-	
+
 }
 
-func (c *DBClient) InsertUser() {
+// InsertUser inserts new registered user to the database
+func (c *DBClient) InsertUser(username, passwordhash, email string) {
+	newUser := User{
+		Username:     username,
+		PasswordHash: passwordhash,
+		Email:        email,
+	}
+
+	collection := c.Database("korero").Collection("users")
+	_, err := collection.InsertOne(context.TODO(), newUser)
+	if err != nil {
+		log.Fatalf("[-] InsertOne User error: %v", err)
+	}
 
 }
 
 // GetExpectedPassword queries password from the database based on given username
-func (c *DBClient) GetExpectedPassword(username string) string {
+func (c *DBClient) GetExpectedPasswordHash(username string) string {
+	var expectedPasswordHash string
+
 	collection := c.Database("korero").Collection("users")
-	if err = collection.FindOne()
+	result := collection.FindOne(context.Background(), bson.M{"username": username})
+
+	_, err := result.Decode(&expectedPasswordHash)
+
+	return expectedPasswordHash
 
 }
 
 // CheckIfUsernameTakes queries given username from the database and check if such entry already exists
 func (c *DBClient) CheckIfUsernameTaken(username string) bool {
-	collection := c.Database("korero").Collection("users")
+
 }
