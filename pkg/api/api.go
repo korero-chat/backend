93f1f75c29d379e54008fb70a8ffc567ff74c9cf
hyperxpizza <hyperxpizza@gmail.com>
2020-04-02 19:23:08 +0200

    add registeruser api endpoint

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,7 +1,42 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/korero-chat/backend/pkg/crypto"
+	"github.com/korero-chat/backend/pkg/database"
+)
 
 func RegisterUserEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// Get data from json request and store them in user variable
+	var user database.User
+	_ = json.NewDecoder(r.Body).Decode((&user))
+
+	//Check if username already taken
+	result := database.CheckIfUsernameTaken(user.Username)
+	if result == false {
+		w.WriteHeader(http.StatusConflict)
+		return
+	}
+
+	// Hash password
+	passwordhash, err := crypto.HashPassword(user.PasswordHash)
+	if err != nil {
+		log.Fatalf("[-] Error while hashing the password")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	//Insert user into the database
+	database.InsertUser(user.Username, passwordhash, user.Email)
+
+	json.NewEncoder(w).Encode(user)
 
 }
