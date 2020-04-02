dbafafc44bfd566247391e792f3655f5e896c377
hyperxpizza <hyperxpizza@gmail.com>
2020-04-02 13:39:41 +0200

    add test user model

--- pkg/database/models.go
+++ pkg/database/models.go
@@ -1,2 +1,10 @@
 package database
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID           primitive.ObjectID `bson:"_id, omitempty"`
+	Username     string             `json:"username"`
+	PasswordHash string             `json:"passwordhash"`
+	Email        string             `json:"email"`
+}
