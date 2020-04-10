c087c5440f25447101270b8c55dca3a23101dc9f
hyperxpizza <hyperxpizza@gmail.com>
2020-04-10 15:44:56 +0200

    username max length 20

--- models/user.go
+++ models/user.go
@@ -1,13 +1,13 @@
 package models
 
 type User struct {
-	Username string             `json:"username"`
-	Password string             `json:"password"`
-	Email    string             `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
 }
 
 type NewUserRequest struct {
-	Username string `validate:"min=3,max=40,regexp=^[a-zA-Z]*$"`
+	Username string `validate:"min=3,max=20,regexp=^[a-zA-Z]*$"`
 	Password string `validate:"min=8"`
 	Email    string `validate:"regexp=^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$"`
 }
