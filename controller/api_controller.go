0ae023d2f8fc643027fae1f5596e24a825a48fdc
hyperxpizza <hyperxpizza@gmail.com>
2020-04-25 14:16:24 +0200

    getchatbyid api endpoint

--- controller/api_controller.go
+++ controller/api_controller.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/korero-chat/backend/database"
 	"github.com/korero-chat/backend/models"
 	"gopkg.in/validator.v2"
@@ -48,4 +49,25 @@ func CreateChatEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	response.Result = "Chat created successfully"
 	json.NewEncoder(w).Encode(response)
+}
+
+func GetChatEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	var response models.ResponseModel
+
+	params := mux.Vars(r)
+	chat, err := database.GetChatByID(params["id"])
+	if err != nil {
+		w.WriteHeader(500)
+		response.Error = err.Error()
+		json.NewEncoder(w).Encode(response)
+		return
+
+	}
+
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(chat)
+	return
 }
